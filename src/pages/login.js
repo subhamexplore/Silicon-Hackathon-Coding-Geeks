09415820b72f623d6be4f44756aa34dbe7d828c6
Dev Ashrit Behera <devashritcode@gmail.com>
2023-02-06 18:43:07 +0530

    login signup frontend by dev after abhijeet

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,31 +1,51 @@
 import React, { useState } from 'react'
-
+import styles from "../styles/Signup.module.css"
+import Link from 'next/link'
 
 const Login = () => {
   const initialDetails = {
-    email : "",
-    password : ""
+    email: "",
+    password: ""
   }
   const [details, setDetails] = useState(initialDetails);
 
   const handleChange = (e) => {
-    setDetails({...details,[e.target.name] : e.target.value})
+    setDetails({ ...details, [e.target.name]: e.target.value })
   }
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(details);
+    setDetails(initialDetails);
   }
   return (
-    <div className='d-flex justify-content-center align-items-center min-vh-100'>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <input className="form-control" type="email" name="email" id="email" onChange={handleChange} placeholder="Email"/>
+    <div className={`d-flex align-items-center justify-content-center ${styles.boddy}`}>
+      <div className={styles.sin}>
+        <div className="d-flex justify-content-center mt-5">
+          <div className='fs-1 fw-bold'>LOGIN</div>
         </div>
-        <div className="mb-3">
-          <input className="form-control" type="password" id="password" name="password" onChange={handleChange} placeholder="Password"/>
+        <div className="d-flex justify-content-center mb-3">
+          <hr className={styles.hrr} />
         </div>
-        <button type="submit" className="btn btn-primary">Login</button>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <div className="m-4 d-flex justify-content-center align-items-center">
+            <div>
+              <input type="email" className={`form-control signup_input ${styles.inputl} ${styles.input_border}`} name="email" aria-describedby="emailHelp" onChange={handleChange} placeholder="Email" required={true} value={details.email}/>
+            </div>
+          </div>
+          <div className="m-4 d-flex justify-content-center">
+            <div>
+              <input type="password" className={`form-control signup_input ${styles.inputl} ${styles.input_border}`} name="password" onChange={handleChange} placeholder="Password" required={true} value={details.password}/>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center m-5 mb-3">
+            <button type="submit" className={`btn ${styles.btnl} px-2 `}>Login</button>
+          </div>
+        </form>
+        <div className="d-flex justify-content-center">
+          <p >Don't have an account? <Link className={`${styles.register_link}`} href="/signup">Register here</Link></p>
+        </div>
+        <div className="d-flex justify-content-end "><Link className={`${styles.register_link} ${styles.forgot}`} href="/signup">Forgot password</Link></div>
+      </div>
     </div>
   )
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,10 +1,11 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
+import styles from "../styles/Signup.module.css"
 
 const Signup = () => {
-    const initialDetails = {
-    username : "",
-    email : "",
-    password : ""
+  const initialDetails = {
+    username: "",
+    email: "",
+    password: ""
   }
   const [details, setDetails] = useState(initialDetails);
   const [focusedName, setfocusedName] = useState(false);
@@ -12,7 +13,7 @@ const Signup = () => {
   const [focusedPassword, setfocusedPassword] = useState(false);
 
   const handleChange = (e) => {
-    setDetails({...details,[e.target.name] : e.target.value})
+    setDetails({ ...details, [e.target.name]: e.target.value })
   }
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,28 +23,45 @@ const Signup = () => {
     setfocusedEmail(false);
     setfocusedPassword(false);
   }
-  const handleBlur = (setE) =>{
+  const handleBlur = (setE) => {
     setE(true);
   }
   return (
-    <div className='d-flex justify-content-center align-items-center min-vh-100'>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <input type="name" name="username" className="form-control signup_input" onChange={handleChange} placeholder="Name" value={details.username} required={true} pattern="^[A-Za-z0-9 ]{3,30}$"  onBlur={()=>{handleBlur(setfocusedName)}} focused={focusedName.toString()} />
-          <p className="text-danger signup_err_message" >Enter a name of 3 to 30 characters</p>
+    <div className={`d-flex align-items-center justify-content-center ${styles.boddy}`}>
+      <div className={styles.sin}>
+        <div className="d-flex justify-content-center mt-5">
+          <div className='fs-1 fw-bold'>SIGN UP</div>
         </div>
-        <div className="mb-3">
-          <input type="email" className="form-control signup_input" name="email" aria-describedby="emailHelp" onChange={handleChange} placeholder="Email" value={details.email} required={true} pattern="^^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"  onBlur={()=>{handleBlur(setfocusedEmail)}} focused={focusedEmail.toString()}/>
-          <p className="text-danger signup_err_message">Enter a valid email</p>
+        <div className="d-flex justify-content-center mb-3">
+          <hr className={styles.hrr} />
         </div>
-        <div className="mb-3">
-          <input type="password" className="form-control signup_input" name="password" onChange={handleChange} placeholder="Password" value={details.password} required={true} pattern="^[A-Za-z0-9 ]{6,30}$"  onBlur={()=>{handleBlur(setfocusedPassword)}} focused={focusedPassword.toString()}/>
-         <p className="text-danger signup_err_message">Enter a password of atleast 6 characters</p>
-        </div>
-        <button type="submit" className="btn btn-primary">Signup</button>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <div className="m-4 d-flex justify-content-center align-items-center ">
+            <div>
+              <input type="name" name="username" className={`form-control signup_input ${styles.inputl} ${styles.input_border}`} onChange={handleChange} placeholder="Name" value={details.username} required={true} pattern="^[A-Za-z0-9 ]{3,30}$" onBlur={() => { handleBlur(setfocusedName) }} focused={focusedName.toString()} />
+              <p className={`text-danger signup_err_message mt-1 px-2 mb-0 ${styles.error_p}`} >Enter a name of 3 to 30 characters</p>
+            </div>
+          </div>
+          <div className="m-4 d-flex justify-content-center align-items-center">
+            <div>
+              <input type="email" className={`form-control signup_input ${styles.inputl} ${styles.input_border}`} name="email" aria-describedby="emailHelp" onChange={handleChange} placeholder="Email" value={details.email} required={true} pattern="^^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$" onBlur={() => { handleBlur(setfocusedEmail) }} focused={focusedEmail.toString()} />
+              <p className={`text-danger signup_err_message mt-1 px-2 mb-0 ${styles.error_p}`}>Enter a valid email</p>
+            </div>
+          </div>
+          <div className="m-4 d-flex justify-content-center">
+            <div>
+              <input type="password" className={`form-control signup_input ${styles.inputl} ${styles.input_border}`} name="password" onChange={handleChange} placeholder="Password" value={details.password} required={true} pattern="^[A-Za-z0-9 ]{6,30}$" onBlur={() => { handleBlur(setfocusedPassword) }} focused={focusedPassword.toString()} />
+              <p className={`text-danger signup_err_message mt-1 px-2 mb-0 ${styles.error_p}`}>Enter a password of atleast 6 characters</p>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center m-5 mb-5">
+            <button type="submit" className={`btn ${styles.btnl} px-2 `}>Sign Up</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
+
 
 export default Signup
